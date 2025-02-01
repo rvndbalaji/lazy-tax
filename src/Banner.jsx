fe21cd8c188d805d3b786789aef7a4c435295aaa
Aravind <Aravind.B@transunion.com>
2025-02-01 16:10:18 +0530

    Updated tax regime for 2025-2026

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import Container from 'react-bootstrap/Container'
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
 
-class Banner extends React.Component 
-{
-    render() 
-    {
-        return (
-            <Jumbotron fluid id="jumbo">
-            <Container>
-              <h1>Lazy Tax</h1>
-              <p>Tax calculator - because you're bad at math</p>
-              <i>FY 2023 - 2024</i>
-            </Container>
-            <span id="myname">- Aravind Balaji, {new Date().getFullYear()}</span>
-          </Jumbotron>
-        )
-    }
+class Banner extends React.Component {
+  render() {
+    return (
+      <Jumbotron fluid id="jumbo">
+        <Container>
+          <h1>Lazy Tax</h1>
+          <p>Indian Tax calculator - because you're bad at math</p>
+          <i>FY 2025 - 2026</i>
+        </Container>
+        <div style={{ display: "block" }} id="myname">
+          Aravind Balaji, {new Date().getFullYear()}
+        </div>
+        <br />
+      </Jumbotron>
+    );
   }
+}
 
-  export default Banner
+export default Banner;
